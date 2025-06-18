10ac82e9271eebd330868f2a30536cad14b78641
Mykhailo Hubko <mixelios@gmail.com>
2025-06-18 21:35:49 +0200

    buttons for fonts choising

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,84 @@
-import {JoinSection} from '../../components/JoinSection/JoinSection'
+import { JoinSection } from '../../components/JoinSection/JoinSection';
+import { useEffect, useState } from 'react';
 
 export const MainPage = () => {
+  const [checkingFont, setCheckingFont] = useState<
+    'Montserrat' | 'Days' | 'Russo' | 'Prosto' | 'Raleway' | ''
+  >('');
+
+  useEffect(() => {
+    if (checkingFont) {
+      document.body.className = 'body';
+      document.body.classList.add(checkingFont);
+    }
+  }, [checkingFont]);
+
   return (
     <>
-      <h1 className='pageTitle'>Gloryphonic main page</h1>
+      <h1 className="pageTitle">Gloryphonic main page</h1>
       <JoinSection />
+      <div className="" style={{ display: 'flex', gap: '20px', fontSize: '32px' }}>
+        <button
+          onClick={() => {
+            setCheckingFont('Montserrat');
+          }}
+          style={{
+            fontFamily: 'Montserrat',
+            fontSize: '32px',
+          }}
+          className="MontserratBtn"
+        >
+          Montserrat
+        </button>
+        <button
+          onClick={() => {
+            setCheckingFont('Days');
+          }}
+          style={{
+            fontFamily: 'Days One',
+            fontSize: '32px',
+          }}
+          className={'DaysBtn'}
+        >
+          Days One
+        </button>
+        <button
+          onClick={() => {
+            setCheckingFont('Prosto');
+          }}
+          style={{
+            fontFamily: 'Prosto One',
+            fontSize: '32px',
+          }}
+          className={'ProstoBtn'}
+        >
+          Prosto One
+        </button>
+        <button
+          onClick={() => {
+            setCheckingFont('Russo');
+          }}
+          style={{
+            fontFamily: 'Russo',
+            fontSize: '32px',
+          }}
+          className={'RussoBtn'}
+        >
+          Russo One
+        </button>
+        <button
+          onClick={() => {
+            setCheckingFont('Raleway');
+          }}
+          style={{
+            fontFamily: 'Raleway',
+            fontSize: '32px',
+          }}
+          className={'RalewayBtn'}
+        >
+          Raleway
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
