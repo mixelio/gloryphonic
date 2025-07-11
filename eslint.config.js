599be3dcf1e0acfc076046e076d8401e2b83ab3b
Mykhailo Hubko <mixelios@gmail.com>
2025-07-11 10:53:28 +0200

    test 2

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,7 @@ export default tseslint.config({ ignores: ['dist'] }, {
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
+    "eqeqeq": ["error", "always"],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
