a5ee2b600f9697e3bb44397298d40e2ad7a8a206
Mykhailo Hubko <mixelios@gmail.com>
2025-08-21 12:16:17 +0200

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,12 +18,12 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@styles': path.resolve(__dirname, 'src/assets/styles'),
-      '@stylesUtils': path.resolve(__dirname, 'src/assets/styles/utils'),
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@hooks': resolve(__dirname, 'src/hooks'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@styles': resolve(__dirname, 'src/assets/styles'),
+      '@stylesUtils': resolve(__dirname, 'src/assets/styles/utils'),
     },
   },
 });
