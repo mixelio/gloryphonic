e15c753f47251b4157bdf60d5fb44275823dd4a0
Mykhailo Hubko <mixelios@gmail.com>
2025-09-18 06:54:38 +0200

    home page and about us page hero sections have been updated

--- src/components/sections/AboutUsHero/AboutUsHero.tsx
+++ src/components/sections/AboutUsHero/AboutUsHero.tsx
@@ -3,9 +3,9 @@ import { AboutUsSlider } from '../../AboutUsSlider/AboutUsSlider.tsx';
 export const AboutUsHero = () => {
   return (
     <section className={`aboutUsHero`}>
-      <div className="container">
+
         <AboutUsSlider />
-      </div>
+
     </section>
   );
 };
